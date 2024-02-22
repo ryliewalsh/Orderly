48c2d5b55c584525fe9412721d75f6f7a793bddb
ryliewalsh <ryliewalsh@u.boisestate.edu>
2024-02-21 17:50:27 -0700

    first commit

---
+++ home.php
@@ -0,0 +1,16 @@
+/* this will be home page */
+<html>
+  <head>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <div id="content">
+       <h1>Welcome to the Dark Web</h1>
+       <div id="nav">
+         <ol>
+           <li><a href="index.php">Home</a></li>
+           <li><a href="comments.php">Reviews (feelings)</a></li>
+           <li><a href="not-stolen.php">Not-stolen</a></li>
+           <li><a href="nice-people.php">Nice People</a></li>
+           <li><a href="about.php">About</a></li>
+         </ol>
---
+++ index.php
@@ -0,0 +1,2 @@
+<?php include_once("index.html"); ?>
+/* this will be index */
