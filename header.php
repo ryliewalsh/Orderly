6199f38fd89618b8dc2eb73061e229c4d4a7d7f4
ryliewalsh <ryliewalsh@u.boisestate.edu>
2024-02-21 19:05:20 -0700

    nav added

--- header.php
+++ header.php
@@ -2,4 +2,18 @@
   <head>
     <link rel="stylesheet" href="style.css">
   </head>
-  
+  <body>
+    <header class="header">
+        <div class="box-left">LOGO</div>
+        <div class="box-right">User profile</div>
+    </header>
+        <div id="nav">
+            <ol>
+                <li><a href="toPay.php"></a>To Pay</li>
+                <li><a href="comments.php">To Plan</a></li>
+                <li><a href="not-stolen.php">To Do</a></li>
+                <li><a href="about.php">About</a></li>
+            </ol>
+        </div>
+</body>
+</html>
