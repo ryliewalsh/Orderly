17f2d547a5b22ae11d56920dca2bd3e7f732ac14
ryliewalsh <ryliewalsh@u.boisestate.edu>
2024-02-21 20:55:05 -0700

    f

--- index.php
+++ index.php
@@ -1,84 +1,10 @@
 <?php include_once("header.php"); ?>
 
-<div id="text">
-<!DOCTYPE html>
 <html>
-<head>
-<style>
-* {box-sizing: border-box;}
-ul {list-style-type: none;}
-body {font-family: Verdana, sans-serif;}
-
-.month {
-  padding: 70px 25px;
-  width: 100%;
-  background: #A57959;
-  text-align: center;
-}
-
-.month ul {
-  margin: 0;
-  padding: 0;
-}
-
-.month ul li {
-  color: white;
-  font-size: 20px;
-  text-transform: uppercase;
-  letter-spacing: 3px;
-}
-
-.month .prev {
-  float: left;
-  padding-top: 10px;
-}
-
-.month .next {
-  float: right;
-  padding-top: 10px;
-}
-
-.weekdays {
-  margin: 0;
-  padding: 10px 0;
-  background-color: #ddd;
-}
-
-.weekdays li {
-  display: inline-block;
-  width: 13.6%;
-  color: #666;
-  text-align: center;
-}
-
-.days {
-  padding: 10px 0;
-  background: #eee;
-  margin: 0;
-}
-
-.days li {
-  list-style-type: none;
-  display: inline-block;
-  width: 13.6%;
-  text-align: center;
-  margin-bottom: 5px;
-  font-size:12px;
-  color: #777;
-}
-
-.days li .active {
-  padding: 5px;
-  background: #A57959;
-  color: white !important
-}
-
-
-</style>
-</head>
-<body>
-
-<h1>CSS Calendar</h1>
+  <head>
+    <link rel="stylesheet" href="calendar.css">
+  </head>
+  <body>
 
 <div class="month">      
   <ul>
