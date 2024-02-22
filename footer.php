e879cf6b083f4457f35e0281fdeb6913a9c83c4c
ryliewalsh <ryliewalsh@u.boisestate.edu>
2024-02-21 19:27:21 -0700

    add footer spacing fix

---
+++ footer.php
@@ -0,0 +1,6 @@
+</div>
+     <hr/>
+     <span id="footer">Copyright 2024 Rylie Walsh</span>
+    </div>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -1,2 +1,5 @@
 <?php include_once("index.html"); ?>
 
+
+
+<?php require_once "footer.php"; ?>
