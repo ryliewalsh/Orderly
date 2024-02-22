ea38e919d9e9d2e4f437e6a0c070357b031f8b78
ryliewalsh <ryliewalsh@u.boisestate.edu>
2024-02-21 21:04:16 -0700

    task

--- plan.php
+++ plan.php
@@ -1,1 +1,15 @@
 <?php include_once("index.php"); ?>
+<html>
+  <head>
+    <link rel="stylesheet" href="task.css">
+  </head>
+<body>
+  <h2>Task List</h2>
+  <ul id="task-list">
+    <li class="task-item">Task 1</li>
+    <li class="task-item">Task 2</li>
+    <li class="task-item">Task 3</li>
+    <!-- Additional tasks can be added as needed -->
+  </ul>
+</body>
+</html>
