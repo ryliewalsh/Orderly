f20d6b128cdd748e970a75675778e47d7f8de366
ryliewalsh <ryliewalsh@u.boisestate.edu>
2024-04-15 17:03:03 -0600

    yikes

--- header.php
+++ header.php
@@ -1,15 +1,14 @@
-<?php
-session_start();
-?>
-<!DOCTYPE html>
 <html>
 <head>
     <link rel="stylesheet" href="style.css">
     <link rel="icon" type="image/svg+xml" href="/favicon.svg">
+
 </head>
 <body>
+<?php session_start(); ?>
 <div class="header">
-    <a class="logo" href="index.php">Orderly.</a>
+
+    <a class = "logo" href="index.php">Orderly.</a>
     <a class="overview" href="index.php">Overview</a>
     <a class="pay" href="pay.php">Pay</a>
     <a class="plan" href="plan.php">Plan</a>
@@ -17,8 +16,10 @@ session_start();
     <a class="about" href="about.php">About</a>
 
     <div class="header-right">
+
         <a class="user" href="userSettings.php">Welcome home, <span class="username">USER</span></a>
     </div>
 </div>
 </body>
+
 </html>
