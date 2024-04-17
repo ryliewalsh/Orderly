07d6ddc05e83a92d0517b20898f75c308274704b
ryliewalsh <ryliewalsh@u.boisestate.edu>
2024-04-17 13:26:35 -0600

    loginbackbuttonstop

--- header.php
+++ header.php
@@ -5,13 +5,22 @@
 
 </head>
 <body>
-<?php session_start();
-if (!isset($_SESSION['authenticated']) && basename($_SERVER['PHP_SELF']) !== 'login.php' ) {
-    // Redirect the user to the login/signup page
+<?php
+session_start();
+
+// Check if the user is not authenticated and the current page is not the login page
+if (!isset($_SESSION['authenticated']) && basename($_SERVER['PHP_SELF']) !== 'login.php') {
+    // Redirect the user to the home page or login page
     header("Location: home.php");
     exit();
+}
 
-}?>
+// If the user is on the login page and is already authenticated, redirect to the home page
+if (isset($_SESSION['authenticated']) && basename($_SERVER['PHP_SELF']) === 'login.php') {
+    header("Location: home.php");
+    exit();
+}
+?>
 <div class="header">
 
     <a class = "logo" href="index.php">Orderly.</a>
