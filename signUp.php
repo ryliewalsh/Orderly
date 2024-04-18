94d7b8d576c95d94634da668acc167e4a4333026
ryliewalsh <ryliewalsh@u.boisestate.edu>
2024-04-18 13:55:15 -0600

    fixwl

--- signUp.php
+++ signUp.php
@@ -1,14 +1,3 @@
-<?php
-session_start();
-
-if (isset($_SESSION['error_messages'])) {
-    foreach ($_SESSION['error_messages'] as $error) {
-        echo "<div class='message'>$error</div>";
-    }
-    unset($_SESSION['error_messages']);
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,30 +7,28 @@ if (isset($_SESSION['error_messages'])) {
     <title>Sign Up</title>
 </head>
 <body>
-<div class="header">
-    <div class="logo" >Orderly.</div>
-</div>
-<div class =wallpaper>
-<div class="signup-form">
-<h2>Sign Up</h2>
+<div class="wallpaper">
+    <div class="header">
+        <div class="logo">Orderly.</div>
+    </div>
+    <div class="signup-form">
+        <h2>Sign Up</h2>
+        <form action="signUp_handler.php" method="post">
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" value="<?php echo isset($_SESSION['inputs']['email']) ? htmlspecialchars($_SESSION['inputs']['email']) : ''; ?>"><br><br>
 
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" value="<?php echo isset($_SESSION['inputs']['username']) ? htmlspecialchars($_SESSION['inputs']['username']) : ''; ?>"><br><br>
 
-<form action="signUp_handler.php" method="post">
-    <label for="email">Email:</label>
-    <input type="email" id="email" name="email" value="<?php echo isset($_SESSION['inputs']['email']) ? htmlspecialchars($_SESSION['inputs']['email']) : ''; ?>"><br><br>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password"><br><br>
 
-    <label for="username">Username:</label>
-    <input type="text" id="username" name="username" value="<?php echo isset($_SESSION['inputs']['username']) ? htmlspecialchars($_SESSION['inputs']['username']) : ''; ?>"><br><br>
+            <label for="first_name">First Name:</label>
+            <input type="text" id="first_name" name="first_name" value="<?php echo isset($_SESSION['inputs']['first_name']) ? htmlspecialchars($_SESSION['inputs']['first_name']) : ''; ?>"><br><br>
 
-    <label for="password">Password:</label>
-    <input type="password" id="password" name="password"><br><br>
-
-    <label for="first_name">First Name:</label>
-    <input type="text" id="first_name" name="first_name" value="<?php echo isset($_SESSION['inputs']['first_name']) ? htmlspecialchars($_SESSION['inputs']['first_name']) : ''; ?>"><br><br>
-
-    <button type="submit">Sign Up</button>
-</form>
-</div>
+            <button type="submit">Sign Up</button>
+        </form>
+    </div>
 </div>
 </body>
 </html>
