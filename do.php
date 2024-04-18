be21392cdf7d2758945632b0da6d6f9256a7ee13
ryliewalsh <ryliewalsh@u.boisestate.edu>
2024-04-18 14:26:24 -0600

    k

--- do.php
+++ do.php
@@ -1,24 +1,21 @@
-<?php include_once("header.php");
-
-
-?>
+<?php include_once("header.php"); ?>
 
     <html>
     <head>
-        <title>To Do</title>
+        <title>To Pay</title>
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
 
     <div class="wallpaper">
         <large-box>
+            <large-box class="title">
                 <h1>Agenda</h1>
-
                 <table>
                     <thead>
                     <tr>
-                        <th>Task</th>
-                        <th>Do By</th>
+                        <th>Description</th>
+                        <th>Due By</th>
                     </tr>
                     </thead>
                     <?php
@@ -37,10 +34,13 @@
                     }
                     ?>
                 </table>
+
+            </large-box>
         </large-box>
+
         <div class="vertical">
             <div class="small-box">
-                <div class="label-box">To Do Today</div>
+                <div class="label-box">Due Today</div>
                 <?php
                 require_once "DAO.php";
                 $dao = new DAO();
@@ -59,10 +59,10 @@
 
             <div class="small-box">
                 <div class="label-box">Add a Task</div>
-                <div class="content-box"> <?php include_once("addChore.php");?></div>
+                <div class="content-box"> <?php include_once("addChore.php");?></>
             </div>
         </div>
-
     </div>
     </body>
+    </html>
 <?php require_once "footer.php"; ?>
