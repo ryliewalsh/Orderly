88d7f0ce9ae9ac957b3c851087392b6d782147d7
ryliewalsh <ryliewalsh@u.boisestate.edu>
2024-04-18 17:12:53 -0600

    k

--- signUp.php
+++ signUp.php
@@ -1,4 +1,11 @@
-
+<?php
+if (isset($_SESSION['error_messages'])) {
+    foreach ($_SESSION['error_messages'] as $error) {
+        echo "<div class='message'>$error</div>";
+    }
+    unset($_SESSION['error_messages']);
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
