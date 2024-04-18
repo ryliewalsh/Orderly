355e727c7c6606a9c5218a5e244465c3939e8bc5
ryliewalsh <ryliewalsh@u.boisestate.edu>
2024-04-18 10:03:11 -0600

    g

--- login_handler.php
+++ login_handler.php
@@ -13,15 +13,15 @@ if ($user && password_verify($password, $user['password_hash'])) {
     $_SESSION['username'] = $username;
     $_SESSION['user_id'] = $user['user_id'];
 
-    if ($user['household_id'] === null) {
-        // If the user's household_id is NULL, redirect to the join household page
-        header("Location: https://orderly-b0075f006315.herokuapp.com/joinHousehold.php");
-        exit();
-    } else {
+//    if ($user['household_id'] === null) {
+//        // If the user's household_id is NULL, redirect to the join household page
+//        header("Location: https://orderly-b0075f006315.herokuapp.com/joinHousehold.php");
+//        exit();
+
         // If the user already belongs to a household, redirect to another page
         header("Location: https://orderly-b0075f006315.herokuapp.com/index.php");
         exit();
-    }
+
 } else {
     // Invalid credentials, redirect to the login page
     header("Location: https://orderly-b0075f006315.herokuapp.com/login.php");
