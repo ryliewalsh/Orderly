17df5716bfce2f2052cedee52262498aac07ffd9
ryliewalsh <ryliewalsh@u.boisestate.edu>
2024-04-18 17:22:41 -0600

    p

--- signUp.php
+++ signUp.php
@@ -12,27 +12,10 @@ session_start();
 </head>
 <body>
 
-<html>
-<head>
-    <link rel="stylesheet" href="style.css">
-    <link rel="icon" type="image/svg+xml" href="/favicon.svg">
-</head>
-<body>
-<div class="header">
-    <a class="logo" href="index.php">Orderly.</a>
-    <a class="overview" href="index.php">Overview</a>
-    <a class="pay" href="pay.php">Pay</a>
-    <a class="plan" href="plan.php">Plan</a>
-    <a class="do" href="do.php">Do</a>
-    <a class="about" href="about.php">About</a>
-    <div class="header-right">
-        <a class="user" href="userSettings.php">Welcome home, <span class="username">User</span></a>
+    <div class="header">
+        <div class="logo">Orderly.</div>
     </div>
-</div>
-</body>
-</html>
-
-<?php
+    <?php
     if (isset($_SESSION['error_messages'])) {
         foreach ($_SESSION['error_messages'] as $error) {
             echo "<div class='message'>$error</div>";
