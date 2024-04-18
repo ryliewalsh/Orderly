d60e7426d91c2426baa9321d13ef1484d6e18bf9
ryliewalsh <ryliewalsh@u.boisestate.edu>
2024-04-18 15:42:39 -0600

    f

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <body>
 
 <div class="wallpaper">
-    <large-box >
+    <large-box>
         <?php include_once("calendar.php"); ?>
     </large-box>
     <div class="vertical">
@@ -24,10 +24,10 @@
                     echo "Congrats nothing to do!";
                 } else {
                     foreach ($chores as $chore) {
-                        echo "<div>{$chore['description']}</div>";
+                        echo "<div class='item-box'><div class='small-item'></div><div>{$chore['description']}</div></div>";
                     }
                     foreach ($bills as $bill) {
-                        echo "<div>{$bill['description']}</div>";
+                        echo "<div class='item-box'><div class='small-item'></div><div>{$bill['description']}</div></div>";
                     }
                 }
                 ?>
@@ -44,10 +44,10 @@
                     echo "All caught up!";
                 } else {
                     foreach ($upcomingChores as $chore) {
-                        echo "<div>{$chore['description']}</div>";
+                        echo "<div class='item-box'><div class='small-item'></div><div>{$chore['description']}</div></div>";
                     }
                     foreach ($upcomingBills as $bill) {
-                        echo "<div>{$bill['description']}</div>";
+                        echo "<div class='item-box'><div class='small-item'></div><div>{$bill['description']}</div></div>";
                     }
                 }
                 ?>
