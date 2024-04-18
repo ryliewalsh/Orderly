a478acc419e4d021b4a404e5e4513b4f03c3f134
ryliewalsh <ryliewalsh@u.boisestate.edu>
2024-04-18 11:00:49 -0600

    j

--- login.php
+++ login.php
@@ -18,6 +18,8 @@
 
 
                 this.submit();
+                $('#username').val('');
+                $('#password').val('');
             });
         });
     </script>
--- login_handler.php
+++ login_handler.php
@@ -2,7 +2,6 @@
 session_start();
 
 if (isset($_POST['username']) && isset($_POST['password'])) {
-    // If the username and password are set, attempt to authenticate the user
     $username = $_POST['username'];
     $password = $_POST['password'];
 
@@ -12,23 +11,22 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
     $user = $dao->getUser($username);
 
     if ($user && password_verify($password, $user['password_hash'])) {
-        // If authentication is successful, set session variables
+
         $_SESSION['authenticated'] = true;
         $_SESSION['username'] = $username;
         $_SESSION['user_id'] = $user['user_id'];
 
-        // Clear the password from the POST data
+
         unset($_POST['password']);
     } else {
-        // If authentication fails, set a session variable to indicate login failure
+
         $_SESSION['login_failed'] = true;
     }
 }
 
-// Unset all session variables if the login page is loaded
 
 
-// Redirect the user based on authentication status
+
 if ($_SESSION['authenticated']) {
     header("Location: https://orderly-b0075f006315.herokuapp.com/index.php");
 } else {
