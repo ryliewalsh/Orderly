4d02f6f2830f88b4825d8ae2d3d61feeccceea4f
ryliewalsh <ryliewalsh@u.boisestate.edu>
2024-04-18 13:36:38 -0600

    j

--- signUp.php
+++ signUp.php
@@ -23,7 +23,7 @@ if (isset($_SESSION['error_messages'])) {
 
 <form action="signUp_handler.php" method="post">
     <label for="email">Email:</label>
-    <input type="email" id="email" name="email" >"><br><br>
+    <input type="email" id="email" name="email" value="<?php echo isset($_SESSION['inputs']['email']) ? htmlspecialchars($_SESSION['inputs']['email']) : ''; ?>"><br><br>
 
     <label for="username">Username:</label>
     <input type="text" id="username" name="username" value="<?php echo isset($_SESSION['inputs']['username']) ? htmlspecialchars($_SESSION['inputs']['username']) : ''; ?>"><br><br>
