63e8f03fb16ed838739fe197994c0871c9076d7a
ryliewalsh <ryliewalsh@u.boisestate.edu>
2024-04-18 10:55:33 -0600

    l

--- login_handler.php
+++ login_handler.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-session_unset();
 
 if (isset($_POST['username']) && isset($_POST['password'])) {
+    // If the username and password are set, attempt to authenticate the user
     $username = $_POST['username'];
     $password = $_POST['password'];
 
@@ -12,19 +12,23 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
     $user = $dao->getUser($username);
 
     if ($user && password_verify($password, $user['password_hash'])) {
-
+        // If authentication is successful, set session variables
         $_SESSION['authenticated'] = true;
         $_SESSION['username'] = $username;
         $_SESSION['user_id'] = $user['user_id'];
 
-
+        // Clear the password from the POST data
         unset($_POST['password']);
     } else {
+        // If authentication fails, set a session variable to indicate login failure
         $_SESSION['login_failed'] = true;
     }
 }
 
+// Unset all session variables if the login page is loaded
+session_unset();
 
+// Redirect the user based on authentication status
 if ($_SESSION['authenticated']) {
     header("Location: https://orderly-b0075f006315.herokuapp.com/index.php");
 } else {
