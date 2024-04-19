a1665206d649f6024c8c3326ee8ccd7a75425498
ryliewalsh <ryliewalsh@u.boisestate.edu>
2024-04-19 17:17:29 -0600

    homep

--- home.php
+++ home.php
@@ -2,13 +2,18 @@
 <html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome</title>
 
 </head>
+
 <body>
-<a href = 'signUp.php'>Sign up</a>
+<div class="header">
+    <a class="logo" href="index.php">Orderly.</a>
+</div>
+<a href= 'signUp.php'>Sign up</a>
 <a href = 'login.php'>Log in</a>
 </body>
 </html>
