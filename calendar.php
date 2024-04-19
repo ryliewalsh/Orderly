3825ccb5640dae553568acf7338e002691020b65
ryliewalsh <ryliewalsh@u.boisestate.edu>
2024-04-19 16:26:01 -0600

    addbil

--- calendar.php
+++ calendar.php
@@ -1,20 +1,33 @@
+
 <head>
-    <link rel="stylesheet" href="style.css">
-</head>
-<html lang='en'>
-<head>
+
+    <?php
+    require_once "DAO.php";
+    $dao = new DAO();
+    $chores = $dao->getTodaysChores();
+    $bills = $dao->getTodaysBills();
+
+
+    ?>
     <meta charset='utf-8' />
     <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.11/index.global.min.js'></script>
     <script>
-
         document.addEventListener('DOMContentLoaded', function() {
             var calendarEl = document.getElementById('calendar');
+
             var calendar = new FullCalendar.Calendar(calendarEl, {
-                initialView: 'dayGridMonth'
+                initialView: 'dayGridMonth',
+                initialDate: '2024-03-07',
+                headerToolbar: {
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                },
+                events: <?php echo json_encode($bills); ?> // Replace with dynamic PHP data
             });
+
             calendar.render();
         });
-
     </script>
 </head>
 <body>
