550b07196bbb9a0cb39ef039c6d3ea5dc0a20cb6
ryliewalsh <ryliewalsh@u.boisestate.edu>
2024-04-22 13:23:00 -0600

    g

--- signUp.php
+++ signUp.php
@@ -50,6 +50,7 @@ if (isset($_SESSION['error_messages'])) {
                 <button type="submit">Sign Up</button>
 
             </form>
+        <a class = 'content-box' href="login.php">Log in</a>
         </div>
  </div>
 </div>
