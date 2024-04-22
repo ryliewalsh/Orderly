efa9fe1ac2d7f723894b3e72602fe75a7abfd0fe
ryliewalsh <ryliewalsh@u.boisestate.edu>
2024-04-22 12:22:57 -0600

    g

--- removeEvent.php
+++ removeEvent.php
@@ -12,38 +12,12 @@ if (isset($_POST['bill_id'])) {
 
 
     try {
-        $updatedBills = $dao->getBills();
-        session_start();
-
-
-        require_once "DAO.php";
-        $dao = new DAO();
-
-        header('Content-Type: application/json');
-
-        if (isset($_POST['bill_id'])) {
-            $bill_id = $_POST['bill_id'];
-
-
-            try {
-                $updatedBills = $dao->getBills();
-                echo json_encode(['success' => true, 'bills' => $updatedBills]);
-
-            } catch (Exception $e) {
-                echo json_encode(['success' => false, 'message' => 'Failed to pay bill: ' . $e->getMessage()]);
-            }
-        } else {
-            echo json_encode(['success' => false, 'message' => 'Bill ID not provided']);
-        }
-
-
-        echo json_encode(['success' => true, 'bills' => $updatedBills]);
-
+        $dao->payBill($bill_id);
+        echo json_encode(['success' => true]);
     } catch (Exception $e) {
         echo json_encode(['success' => false, 'message' => 'Failed to pay bill: ' . $e->getMessage()]);
     }
 } else {
     echo json_encode(['success' => false, 'message' => 'Bill ID not provided']);
 }
-
 ?>
