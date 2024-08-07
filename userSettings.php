9f6d400c50f3f159fb08237bada91f520e657240
Rylie Walsh <62161188+ryliewalsh@users.noreply.github.com>
2024-08-07 23:36:58 +0000

    testing pay.php layout on userSettings

--- userSettings.php
+++ userSettings.php
@@ -6,43 +6,57 @@
     </head>
     <body>
 
-    <div class="wallpaper">
-         <large-box>
-              <div class="title">
-                <h1>Your Household</h1>
-                <table>
+<div class="wallpaper">
+    <large-box>
+        <div class="title">
+            <h1>Your Household</h1>
 
-                    <?php
+            <table>
 
-                    $lines = $dao->getBills();
+                <?php
 
-                    if (count($lines) == 0) {
-                        echo "<div class='item-box'><div class='item'><span>Congrats, nothing due!</span></div></div>";
-                    } else {
-                        echo "<div class='item-box'>";
-                        foreach ($lines as $line) {
-                            echo "<div class='item'><span>{$line['description']}</span>
-                                <span>{$line['amount']}</span><span>{$line['due_date']}</span>
-                                
-                                <button class='trigger-function-button' b_id='{$line['bill_id']}'>Mark as Paid</button></div>";
-                        }
-                        echo "</div>";
+                $lines = $dao->getHouseId();
+
+                if (count($lines) == 0) {
+                    echo "<div class='item-box'><div class='item'><span>None set</span></div></div>";
+                } else {
+                    echo "<div class='item-box'>";
+                    foreach ($lines as $line) {
+                        echo "<div class='item'><span>{$line['description']}</span>
+                               <span>{$line['amount']}</span><span>{$line['due_date']}</span>
+                               
+                               <button class='trigger-function-button' b_id='{$line['bill_id']}'>Mark as Paid</button></div>";
                     }
-                    ?>
-                </table>
-            </div>
-         </large-box>
-        <div class="vertical">
-            <div class="small-box">
-                <div class="label-box">Your info</div>
-                <div class="content-box">*****</div>
-            </div>
-            <div class="small-box">
-                <div class="label-box">Account</div>
-                <a class = "homeButton" href="signOut.php">Sign Out.</a>
-                <div class="content-box">Delete account</div>
-            </div>
+                    echo "</div>";
+                }
+                ?>
+            </table>
+        </div>
+    </large-box>
+
+    <div class="vertical">
+        <div class="small-box">
+            <div class="label-box">Due Today</div>
+            <?php
+            require_once "DAO.php";
+            $dao = new DAO();
+            $lines = $dao->getTodaysBills();
+            if (count($lines) == 0) {
+                echo "<div class='item-box'><div class='item'><span>Caught up for the day!</span></div></div>";
+            } else {
+                echo "<div class='item-box'>";
+                foreach ($lines as $line) {
+                    echo "<div class='item'><span>{$line['description']}</span><span>{$line['amount']}</span><span>{$line['due_date']}</span></div>";
+                }
+                echo "</div>";
+            }
+            ?>
+        </div>
+
+        <div class="small-box">
+            <div class="label-box">Add an Expense</div>
+            <div class="content-box"> <?php include_once("addBill.php");?></div>
         </div>
     </div>
-    </body>
+</div>
 <?php require_once "footer.php"; ?>
